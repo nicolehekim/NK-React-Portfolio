a34c14f3f4957dd55d72240da1544876b61e3ada
Nicole Kim <nicolehekim@gmail.com>
2024-07-08 18:46:32 -0700

    added to individual project cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import pastProjects from './pastProjects'
 import '../assets/Project.css'
 
 
@@ -12,9 +13,30 @@ function Project() {
               title: 'Catzzeria',
               description: "Cat-Based Clicker Game",
               deployedLink: 'https://alfwall.github.io/catzzeria/',
-              githubLink: ''
-            }
-          ]}
+              githubLink: 'https://github.com/alfwall/catzzeria',
+            },
+            {
+              title: 'Reading Roundtables',
+              description: "Booksearch Website Using Google API",
+              deployedLink: 'https://reading-roundtable-a1d1097f86e5.herokuapp.com/login',
+              githubLink: 'https://github.com/AdjoaHackman/reading-roundtable',
+            },
+            {
+              title: 'MVC Blogsite',
+              description: "User-Friendly Blogsite with Posting/Login Features",
+              deployedLink: 'https://nks-mvc-blogsite-bab7d4b0eabe.herokuapp.com/',
+              githubLink: 'https://nks-mvc-blogsite-bab7d4b0eabe.herokuapp.com/',
+            },
+          ].map((project, index) => (
+            <div className='col-12 col-md-6 col-lg-4' key={index}>
+              <pastProjects 
+                title={project.title}
+                description={project.description}
+                deployedLink={project.deployedLink}
+                githubLink={project.githubLink}
+              ></pastProjects>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/pastProjects.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, ListGroup } from 'react-bootstrap'
+
+
+function pastProjects(props) {
+    return(
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>{props.description}</Card.Text>
+            </Card.Body>
+            <Card.Body>
+                <Card.Link href={props.deployedLink}>Deployed Link</Card.Link>
+                <Card.Link href={props.githubLink}>Github Link</Card.Link>
+            </Card.Body>
+        </Card>
+    )
+};
+
+export default pastProjects
